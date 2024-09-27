9e9edc28e106e530885b269f0c68ee66bc1618fe
BrianZ95 <brianz0905@gmail.com>
2024-09-27 03:10:44 +0000

    hi

--- Feeder.java
+++ Feeder.java
@@ -6,53 +6,61 @@ public class Feeder {
      */
     private int currentFood;
 
-    public Feeder() {}
-
-    public Feeder(int c)
-    {
-        currentFood=c;
+    public Feeder() {
+        currentFood = 0; // Default to 0 or set to any desired initial value
     }
+
+    public Feeder(int c) {
+        currentFood = c; // Initialize currentFood
+    }
+
     /**
      * Simulates one day with numBirds birds or possibly a bear at the bird
-     * feeder, as described in part (a) Precondition: numBirds > 0
+     * feeder, as described in part (a). Precondition: numBirds > 0
      */
-    public void simulateOneDay(int numBirds)
-    {
-        if(Math.random() < 0.05) currentFood = 0;
-        else
-        {
-            int eaten = (int) (Math.random() *41) +10;
-            currentFood-=eaten*numBirds;
-            if (currentFood<0) currentFood=10;
+    public void simulateOneDay(int numBirds) {
+        // 5% chance that a bear comes and empties the feeder
+        if (Math.random() < 0.05) {
+            currentFood = 0; // Bear empties the feeder
+        } else {
+            int eaten = 0; // Total food eaten by all birds
+            for (int i = 0; i < numBirds; i++) {
+                eaten += (int) (Math.random() * 41) + 10; // Random food per bird (10 to 50)
+            }
+            currentFood -= eaten; // Reduce current food by the total eaten
+
+            // Ensure currentFood does not go below zero
+            if (currentFood < 0) {
+                currentFood = 0; // Feeder is emptied
+            }
         }
     }
 
     /**
      * Returns the number of days birds or a bear found food to eat at the
-     * feeder in this simulation, as described in part (b) Preconditions:
+     * feeder in this simulation, as described in part (b). Preconditions:
      * numBirds > 0, numDays > 0
      */
-    public int simulateManyDays(int numBirds, int numDays) 
-    {
-        int days=0;
-        while(numDays>0)
-        {
-         numDays--;
-         simulateOneDay(numBirds);
-         days++;
+    public int simulateManyDays(int numBirds, int numDays) {
+        int days = 0; // Count of days with food
+
+        while (numDays > 0) {
+            numDays--; // Decrement number of days
+            if (currentFood > 0) { // Check if there's food to simulate
+                simulateOneDay(numBirds);
+                days++; // Increment days only if there's food available
+            } else {
+                break; // Stop if no food is left
+            }
         }
-        if(currentFood==0)
-        return days;
+        return days; // Return the number of days food was found
     }
 
-    public int getCurrentFood()
-    {
+    public int getCurrentFood() {
         return currentFood;
     }
 
-    public String toString()
-    {
+    public String toString() {
         return "Current Food: " + currentFood;
     }
-
 }
--- Main.java
+++ Main.java
@@ -1,9 +1,22 @@
-public class Main
-{
-    
-    public static void main(String[] args)
-    {
-    Feeder i = new Feeder(0);
-    System.out.println(i.simulateManyDays(5, 10));
+public class Main {
+
+    public static void main(String[] args) {
+        Feeder a = new Feeder(2400);
+        System.out.println(a.simulateManyDays(10, 4) + ", " + a.getCurrentFood());
+
+        Feeder b = new Feeder(250);
+        System.out.println(b.simulateManyDays(10, 5) + ", " + b.getCurrentFood());
+
+        Feeder c = new Feeder(0);
+        System.out.println(c.simulateManyDays(5, 10) + ", " + c.getCurrentFood());
+
+        Feeder d = new Feeder(1200);
+        System.out.println(d.simulateManyDays(12, 3) + ", " + d.getCurrentFood());
+
+        Feeder e = new Feeder(700);
+        System.out.println(e.simulateManyDays(5, 6) + ", " + e.getCurrentFood());
+
+        Feeder f = new Feeder(500);
+        System.out.println(f.simulateManyDays(8, 4) + ", " + f.getCurrentFood());
     }
 }
